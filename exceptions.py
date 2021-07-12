a0cc680228f06f3fa9ba01af5b3cfe23255d9339
CryptoNyxz <33389839+CryptoNyxz@users.noreply.github.com>
2021-07-12 08:20:56 -0400

    Modify Exception classes
    Removed unnecessary pass statements

--- exceptions.py
+++ exceptions.py
@@ -14,70 +14,46 @@ __license__ = __license__
 class AuthorizationException(Exception):
     """Exception raised when user failed to be authorized."""
 
-    pass
-
 
 class InvalidCredentials(AuthorizationException):
     """Exception raised when invalid credentials were given."""
-
-    pass
 
 
 class AccountLocked(AuthorizationException):
     """Exception raised when current account is locked."""
 
-    pass
-
 
 class DataFileException(Exception):
     """Exception raised when there is a problem with the app data file used."""
-
-    pass
 
 
 class FormatException(Exception):
     """Exception raised when the data doesn't follow format."""
 
-    pass
-
 
 class InvalidAccountFormat(FormatException):
     """Exception raised when the account bytes doesn't follow format."""
-
-    pass
 
 
 class AccountExists(Exception):
     """Exception raised when account already exists."""
 
-    pass
-
 
 class AccountNotFound(Exception):
     """Exception raised when account doesn't exist."""
-
-    pass
 
 
 class InvalidUsername(Exception):
     """Exception raised when username is invalid."""
 
-    pass
-
 
 class TooManyAttempts(Exception):
     """Exception raised when too many attempts to start a session are made."""
-
-    pass
 
 
 class SessionOngoing(Exception):
     """Exception raised when a session is still ongoing."""
 
-    pass
-
 
 class NoSession(Exception):
     """Exception raised when no session is ongoing yet."""
-
-    pass
